15c64e9141ba235b2d0d632bc7c83427dd19b316
jin <vonprios@gmail.com>
2019-06-01 09:40:35 +0900

    Components Posts add

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Posts extends Component {
+    //함수, 상태 설정
+    render() {
+        //상수 설정
+        return (
+            //html
+            <div>this is the posts</div>
+        );
+    }
+}
+
+export default Posts;
