18cadf10b8c4cb622eb3155981df3b1f09414db3
jin <vonprios@gmail.com>
2019-06-01 09:58:48 +0900

    Redux thunk

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducers from 'reducers';
+import reduxThunk from 'redux-thunk';
 
 export default ({children, initialState = {}}) => {
-    return <div>{children}</div>;
+    const store = createStore(
+        reducers, //사용자 반응 상태
+        initialState, //초기값
+        applyMiddleware(reduxThunk)
+    );
+    return <Provider store={store}>{children}</Provider>;
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,3 +18,5 @@ class App extends Component {
 }
 
 export default App;
+
+//redux - react 의 데이터 상태 추적 라이브러리
